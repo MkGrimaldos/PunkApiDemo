db9e869259559be31bd633f0d8a8cf56fa066c52
MIGUEL ANGEL GRIMALDOS MORENO <miguel.angel.grimaldos.moreno@everis.com>
2021-02-14 12:11:53 +0100

    Restyling of BeerListItem. Now it has a picture of the beer and the title is clearer

--- app/src/main/java/com/mkgrimaldos/punkapidemo/ui/beerlist/BeerListItemViewHolder.kt
+++ app/src/main/java/com/mkgrimaldos/punkapidemo/ui/beerlist/BeerListItemViewHolder.kt
@@ -1,16 +1,25 @@
 package com.mkgrimaldos.punkapidemo.ui.beerlist
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.mkgrimaldos.punkapidemo.R
 import com.mkgrimaldos.punkapidemo.domain.model.BeerDetails
 
 class BeerListItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    var ivBeerIcon: ImageView = itemView.findViewById(R.id.iv_beer_icon)
     var tvName: TextView = itemView.findViewById(R.id.tv_name)
     var tvTagline: TextView = itemView.findViewById(R.id.tv_tagline)
 
     fun bind(beerDetails: BeerDetails) {
+        Glide.with(ivBeerIcon)
+            .load(beerDetails.imageUrl)
+            .centerInside()
+            .placeholder(null)
+            .into(ivBeerIcon)
+
         tvName.text = beerDetails.name
         tvTagline.text = beerDetails.tagline
     }
