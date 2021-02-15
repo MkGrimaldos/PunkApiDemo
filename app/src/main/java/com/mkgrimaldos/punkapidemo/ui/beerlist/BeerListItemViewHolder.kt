e0d79e31277ec7081b74bff12e94aefbeaae86ca
MIGUEL ANGEL GRIMALDOS MORENO <miguel.angel.grimaldos.moreno@everis.com>
2021-02-15 22:19:46 +0100

    Setting gray background for unavailable beers in the list

--- app/src/main/java/com/mkgrimaldos/punkapidemo/ui/beerlist/BeerListItemViewHolder.kt
+++ app/src/main/java/com/mkgrimaldos/punkapidemo/ui/beerlist/BeerListItemViewHolder.kt
@@ -4,6 +4,8 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -14,11 +16,19 @@ class BeerListItemViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
     LayoutInflater.from(parent.context).inflate(R.layout.beer_list_item, parent, false)
 ) {
 
+    var clContainer: ConstraintLayout = itemView.findViewById(R.id.cl_container)
     var ivBeerIcon: ImageView = itemView.findViewById(R.id.iv_beer_icon)
     var tvName: TextView = itemView.findViewById(R.id.tv_name)
     var tvTagline: TextView = itemView.findViewById(R.id.tv_tagline)
 
     fun bind(beerDetails: BeerDetails) {
+        val bgColour = ContextCompat.getColor(
+            clContainer.context,
+            if (beerDetails.isAvailable) android.R.color.white else android.R.color.darker_gray
+        )
+
+        clContainer.setBackgroundColor(bgColour)
+
         Glide.with(ivBeerIcon)
             .load(beerDetails.imageUrl)
             .centerInside()
